0c911234581a4839bbe54576d7adc0a302eeb32f
festion <30810608+festion@users.noreply.github.com>
2025-06-05 13:53:59 -0500

    Add ESLint configuration for TypeScript/JavaScript linting

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+    browser: true
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'output/',
+    '*.min.js',
+    'logs/',
+    'npm_proxy_snapshot/',
+    'repos/',
+    'audit-history/',
+    'frontend/dist/',
+    'frontend/node_modules/',
+    'dashboard/dist/',
+    'dashboard/node_modules/',
+    'gitops_deploy_*.tar.gz'
+  ]
+};
